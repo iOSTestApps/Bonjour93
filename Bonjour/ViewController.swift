de60a5b7a3708d1dc6b4a8ced50dce49e1b8da7a
Romain Pouclet <romain.pouclet@gmail.com>
2018-02-23 09:53:32 -0500

    Create sample project

---
+++ Bonjour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bonjour
+//
+//  Created by Romain Pouclet on 2018-02-23.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
